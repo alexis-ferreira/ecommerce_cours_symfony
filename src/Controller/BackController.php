a20a2dc441acafaedcb8e825558dd911e52d330b
Alexis Ferreira <alexis.ferreira71@gmail.com>
2021-07-08 13:09:41 +0200

    Ajout de la page liste des articles

---
+++ src/Controller/BackController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BackController extends AbstractController
+{
+
+    /**
+     * @Route("/addArticle", name="addArticle")
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function addArticle(Request $request, EntityManagerInterface $manager)
+    {
+
+        $article = new Article(); // Ici on instancie un nouvel objet Article vide que l'on va charger avec les données du formulaire.
+
+        $form = $this->createForm(ArticleType::class, $article); // On fait une instance d'un objet Form qui va controller automatiquement la correspondance des champs de formulaire (contenus dans articleType) avec l'entity Article (contenu dans $article).
+
+        $form->handleRequest($request); // La méthode handlerequest de Form nous permet de préparer la requête.
+
+        if ($form->isSubmitted() && $form->isValid()): // Si le formulaire a été soumis et qu'il est valide (boolean de correspondance généré dans le createForm).
+
+            $article->setCreateAt(new DateTime("now"));
+            $photo = $form->get("photo")->getData(); // On récupère l'input type File photo de notre formulaire. Grace a getData on obtient $_FILE dans son intégralité.
+
+            if ($photo):
+
+                $nomPhoto = date('YmdHis') . uniqid() . $photo->getClientOriginalName(); // Ici on modifie le nom de notre photo avec uniqid(); fonction de php générant une clé de hashage de 10 caractères  aléatoires concaténé avec son nom et la date (heure, minute, seconde), pour s'assurer que la photo soit unique en BDD.
+
+                $photo->move(
+                    $this->getParameter('upload_directory'), $nomPhoto); // Equivalent de move_uploaded_file(), attendant 2 paramètres, la direction de l'upload (Défini dans config/service.yaml) dans le parameters. Puis le nom du fichier à insérer.
+
+                $article->setPhoto($nomPhoto);
+
+                $manager->persist($article); // Le manager Symfony fait le lien entre l'entity et la database via l'ORM (Object Relationnal Mapping) Doctrine. Grâce à la méthode persist(), il conserve en mémoire la requête préparé.
+
+                $manager->flush(); // La méthode flush execute les requêtes en mémoire.
+
+                $this->addFlash("success", "L'article à bien été ajouté");
+                $this->redirectToRoute('addArticle');
+            endif;
+
+        endif;
+
+        return $this->render('back/addArticle.html.twig', [
+
+            "form" => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/listeArticle", name="listeArticle")
+     * @param ArticleRepository $articleRepository
+     * @return Response
+     */
+    public function listeArticle(ArticleRepository $articleRepository)
+    {
+
+        $articles = $articleRepository->findAll();
+
+        return $this->render("back/listeArticle.html.twig", [
+
+            "articles" => $articles
+        ]);
+    }
+}
--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,56 +15,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class FrontController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
-     */
-    public function home()
-    {
-
-        return $this->render("front/home");
-    }
-
-    /**
-     * @Route("/addArticle", name="addArticle")
-     * @param Request $request
-     * @param EntityManagerInterface $manager
+     * @Route("/", name="home")
+     * @param ArticleRepository $articleRepository
      * @return Response
      */
-    public function addArticle(Request $request, EntityManagerInterface $manager)
+    public function home(ArticleRepository $articleRepository) // On injecte en dépendance le repository d'Article pour pouvoir hériter des méthodes présente à l'interieur.
     {
+        // Le repository est obligatoirement appelé pour les requêtes de SELECT
 
-        $article = new Article(); // Ici on instancie un nouvel objet Article vide que l'on va charger avec les données du formulaire.
+        $articles = $articleRepository->findAll();
 
-        $form = $this->createForm(ArticleType::class, $article); // On fait une instance d'un objet Form qui va controller automatiquement la correspondance des champs de formulaire (contenus dans articleType) avec l'entity Article (contenu dans $article).
 
-        $form->handleRequest($request); // La méthode handlerequest de Form nous permet de préparer la requête.
 
-        if ($form->isSubmitted() && $form->isValid()): // Si le formulaire a été soumis et qu'il est valide (boolean de correspondance généré dans le createForm).
+        return $this->render("front/home.html.twig", [
 
-            $article->setCreateAt(new DateTime("now"));
-            $photo = $form->get("photo")->getData(); // On récupère l'input type File photo de notre formulaire. Grace a getData on obtient $_FILE dans son intégralité.
-
-            if ($photo):
-
-                $nomPhoto = date('YmdHis') . uniqid() . $photo->getClientOriginalName(); // Ici on modifie le nom de notre photo avec uniqid(); fonction de php générant une clé de hashage de 10 caractères  aléatoires concaténé avec son nom et la date (heure, minute, seconde), pour s'assurer que la photo soit unique en BDD.
-
-                $photo->move(
-                    $this->getParameter('upload_directory'), $nomPhoto); // Equivalent de move_uploaded_file(), attendant 2 paramètres, la direction de l'upload (Défini dans config/service.yaml) dans le parameters. Puis le nom du fichier à insérer.
-
-                $article->setPhoto($nomPhoto);
-
-                $manager->persist($article); // Le manager Symfony fait le lien entre l'entity et la database via l'ORM (Object Relationnal Mapping) Doctrine. Grâce à la méthode persist(), il conserve en mémoire la requête préparé.
-
-                $manager->flush(); // La méthode flush execute les requêtes en mémoire.
-
-                $this->addFlash("success", "L'article à bien été ajouté");
-                $this->redirectToRoute('addArticle');
-            endif;
-
-        endif;
-
-        return $this->render('front/addArticle.html.twig', [
-
-            "form" => $form->createView()
+            "articles" => $articles
         ]);
     }
 } // FIN DE LA CLASS
